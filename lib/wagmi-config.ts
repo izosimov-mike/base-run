eef4069bd4332668dd17776d78b3c9abbafb749b
Mike <izosimov.m@gmail.com>
2026-02-16 21:27:42 +0300

    added builder code

--- lib/wagmi-config.ts
+++ lib/wagmi-config.ts
@@ -1,6 +1,12 @@
 import { http, createConfig } from 'wagmi'
 import { base, mainnet } from 'wagmi/chains'
 import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector'
+import { Attribution } from 'ox/erc8021'
+
+// ERC-8021 Builder Code data suffix for onchain attribution
+const DATA_SUFFIX = Attribution.toDataSuffix({
+  codes: ['bc_grrqwduu'],
+})
 
 export const config = createConfig({
   chains: [base, mainnet],
@@ -10,7 +16,8 @@ export const config = createConfig({
   },
   connectors: [
     farcasterMiniApp()
-  ]
+  ],
+  dataSuffix: DATA_SUFFIX,
 })
 
 // Export chains for use in other components
